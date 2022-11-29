d416ea6a0b3c7a147fb24d8a981b5060bd42289e
Gregory <gregory@gregory.home>
2022-11-29 13:36:03 +0100

    Update hero components

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,18 +3,21 @@ import videoBg from '../../../public/videoBg.mp4'
 
 const Hero = () => {
   return (
-    <div className="hero-video">
-      <video src={videoBg} autoPlay loop muted></video>
-      <div className="hero-text">
+    // autoPlay loop (Add it later before muted)
+    <>
+      <video src={videoBg} muted id="hero-video"></video>
+      <div className="hero-text-container">
         <h1 className="hero-title">Bienvenue chez ImmoCorps</h1>
         <br></br>
         <p>Se lancer dans l'immobilier, peut être donné à tout le monde</p>
         <br></br>
-        <Link  className="link" to='/register' >Inscrivez-vous</Link>
+        <Link  className="link" to='/register' >Register</Link>
       </div>
-    </div>
+    </>
     
   )
 }
 
 export default Hero
+
+
