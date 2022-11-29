66ff0c27f3d8e38515b2883347540d5bdd9b80ce
Gregory <gregory@gregory.home>
2022-11-29 17:06:59 +0100

    Add section and remove classes

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,18 +4,15 @@ import videoBg from '../../videos/videoBg.mp4'
 const Hero = () => {
   return (
     // autoPlay loop (Add it later before muted)
-    <>
-      <video src={videoBg} muted id="hero-video"></video>
-      <div className="hero-text-container">
+    <section className='hero-container'>
+      <video src={videoBg} muted></video>
+      <div>
         <h1>Bienvenue chez ImmoCorps</h1>
         <p>Se lancer dans l'immobilier, peut être donné à tout le monde</p>
         <Link to='/register' >Register</Link>
       </div>
-    </>
-    
+    </section>
   )
 }
 
 export default Hero
-
-
