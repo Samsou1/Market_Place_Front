533230fce730872e680c1c258ac5c426febabe32
pandaka87 <noellie.peuch2@gmail.com>
2022-11-30 17:10:46 +0100

    WIP SearchBar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -21,7 +21,7 @@ const SearchBar = () => {
           value={search}
           type="text"
           id="search"
-          placeholder="Search"
+          placeholder="Location"
         />
       </div>
     </form>
